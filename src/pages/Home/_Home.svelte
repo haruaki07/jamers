de7cd96a4fd9b1455c5c76529c81fe229711fef7
haruaki07 <misbahulk2236@gmail.com>
2021-04-18 08:23:47 +0700

    use object style

--- src/pages/Home/_Home.svelte
+++ src/pages/Home/_Home.svelte
@@ -3,42 +3,41 @@
   import { theme, tw } from "twind";
   import { push } from "svelte-spa-router";
 
-  const style = tw(css`
-    @apply w-full h-screen text-center relative py-24 sm:py-4 lg:py-24 flex flex-col;
-
-    .title {
-      text-shadow: 0 4px 1px ${theme("colors", ["red", "900"])};
-      @apply text(white 6xl) font-black;
-    }
-
-    .menu {
-      @apply mt-24 w-full space-y-10 flex flex(col grow) items-center justify-center;
-    }
-
-    .btn {
-      transition: 150ms all;
-      will-change: box-shadow, transform;
-      @apply text(3xl white) font-bold px-10 py-3 rounded-lg focus:(outline-none) outline-none border-0;
-      &:hover {
-        @apply bg-opacity-30;
-      }
-
-      &:active {
-        @apply bg-opacity-40 translate-y-1;
-        box-shadow: unset;
-      }
-
-      &-red {
-        box-shadow: 0 4px 1px ${theme("colors", ["red", "300"])};
-        @apply bg(red-600 opacity-20);
-      }
-
-      &-blue {
-        box-shadow: 0 4px 1px ${theme("colors", ["blue", "300"])};
-        @apply bg(blue-600 opacity-20);
-      }
-    }
-  `);
+  const style = tw(
+    css({
+      "@apply":
+        "w-full h-screen text-center relative py-24 sm:py-4 lg:py-24 flex flex-col",
+      ".title": {
+        textShadow: `0 4px 1px ${theme("colors", ["red", "900"])}`,
+        "@apply": "text(white 6xl) font-black",
+      },
+      ".menu": {
+        "@apply":
+          "mt-24 w-full space-y-10 flex flex(col grow) items-center justify-center",
+      },
+      ".btn": {
+        transition: "150ms all",
+        willChange: "box-shadow, transform",
+        "@apply":
+          "text(3xl white) font-bold px-10 py-3 rounded-lg focus:(outline-none) outline-none border-0",
+        "&:hover": {
+          "@apply": "bg-opacity-30",
+        },
+        "&:active": {
+          "@apply": "bg-opacity-40 translate-y-1",
+          boxShadow: "unset",
+        },
+        "&-red": {
+          boxShadow: `0 4px 1px ${theme("colors", ["red", "300"])}`,
+          "@apply": "bg(red-600 opacity-20)",
+        },
+        "&-blue": {
+          boxShadow: `0 4px 1px ${theme("colors", ["blue", "300"])}`,
+          "@apply": "bg(blue-600 opacity-20)",
+        },
+      },
+    })
+  );
 </script>
 
 <div class={style}>
--- src/pages/Settings/_Settings.svelte
+++ src/pages/Settings/_Settings.svelte
@@ -2,19 +2,21 @@
   import { css } from "twind/css";
   import { theme, tw } from "twind";
 
-  const style = tw(css`
-    z-index: 1;
-    @apply w-full h-screen text-center relative select-none py-24 sm:py-4 lg:py-24 flex flex-col;
-
-    .title {
-      text-shadow: 0 4px 1px ${theme("colors", ["red", "900"])};
-      @apply text(white 5xl) sm:text-6xl font-black;
-    }
-
-    .btn-back {
-      @apply mt-24 bg-white rounded-lg p-3 text-red-900 font-bold w-min mx-auto shadow-xl;
-    }
-  `);
+  const style = tw(
+    css({
+      zIndex: 1,
+      "@apply":
+        "w-full h-screen text-center relative select-none py-24 sm:py-4 lg:py-24 flex flex-col",
+      ".title": {
+        textShadow: `0 4px 1px ${theme("colors", ["red", "900"])}`,
+        "@apply": "text(white 5xl) sm:text-6xl font-black",
+      },
+      ".btn-back": {
+        "@apply":
+          "mt-24 bg-white rounded-lg p-3 text-red-900 font-bold w-min mx-auto shadow-xl",
+      },
+    })
+  );
 </script>
 
 <div class={style}>
